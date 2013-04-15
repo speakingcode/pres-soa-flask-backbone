52751335560aed7a405fd34dddb3d617a9c4784f
speakingcode <dlissner@gmail.com>
2013-04-15 03:52:58 -0500

    added notes.py flask-based backend

---
+++ notes.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, json, request, Response
+
+app = Flask(__name__)
+
+
+allnotes = []
+index = 0
+
+@app.route('/notes', methods=['GET', 'POST'])
+def notes():
+  if request.method == 'GET':
+    return get_all_notes()
+  elif request.method == 'POST':
+    return create_note(json.loads(request.data))
+
+@app.route('/notes/<int:id>', methods=['GET', 'PUT'])
+def notes_id(id):
+  if request.method == 'GET':
+    return get_note(id)
+  elif request.method == 'PUT':
+    return update_note(id, json.loads(request.data))
+
+def get_all_notes():
+  global allnotes
+  return Response(json.dumps(allnotes), mimetype='application/json')
+
+def create_note(note):
+  global allnotes
+  global index
+  note['id'] = index
+  allnotes.append(note)
+  index = index + 1
+  return jsonify(allnotes[len(allnotes)-1])
+
+def update_note(id, note):
+  global allnotes
+  allnotes[id] = note
+  return jsonify(allnotes[id])
+
+def get_note(id):
+  global allnotes
+  return jsonify(allnotes[id])
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run()
